9657f6bdd3a58f3c15a7b53883b3ea6d00edf21c
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2024-11-08 20:35:20 -0600

    Inventory items can now be clicked

--- secret-to-everyone/src/entities/ui/inventory_menu/inventory_menu.gd
+++ secret-to-everyone/src/entities/ui/inventory_menu/inventory_menu.gd
@@ -11,4 +11,13 @@ func _ready() -> void:
 		var icon = item.item_sprite
 		var item_slot = item_slots[index]
 		item_slot.icon = icon
+		item_slot.index = index
+
+		item_slot.connect("on_inventory_item_clicked", on_inventory_item_clicked.bind())
+
 		index += 1
+
+
+func on_inventory_item_clicked(index: int) -> void:
+	var item_to_show = items[index]
+	print("PRESSED ON ", item_to_show.item_name)
