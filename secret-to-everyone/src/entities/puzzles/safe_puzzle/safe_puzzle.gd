c67238c230087a12194973ebe3c4780f617838a5
Carlos Eduardo Pérez Villanueva <carlospe097@outlook.com>
2024-11-23 17:51:33 -0600

    Add all keys to safe and improve safe 3D model

---
+++ secret-to-everyone/src/entities/puzzles/safe_puzzle/safe_puzzle.gd
@@ -0,0 +1,7 @@
+extends Node3D
+
+
+func _on_a_collider_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			print("PRESSED")
